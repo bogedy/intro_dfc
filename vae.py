ba3ad3d1122e8d3f8cd439032fba0ad801edb645
bogedy <isaiahkriegman@gmail.com>
2019-07-23 15:35:36 -0500

    added mnist option

--- vae.py
+++ vae.py
@@ -1,10 +1,10 @@
 from train_ops import *
 
-os.environ["CUDA_VISIBLE_DEVICES"]="0"
+#os.environ["CUDA_VISIBLE_DEVICES"]="0"
 
 ###########  Parameters  ############
 #folder to save weights and images
-DIR = 'experiment18'
+DIR = 'experimentmnist'
 BATCH_SIZE = 128
 image_size = 192
 epochs = 50
@@ -17,19 +17,32 @@ opt3 = tf.optimizers.Adam(lr)
 log_freq = 100
 kernelsize = 3
 # mode is one of: vae, dfc, combo, fixed, latent
-mode = 'latent'
+mode = 'vae'
+# data is one of: mnist, celeba
+data = 'mnist'
 model = VAE(latent_dim, image_size, mode, kernelsize)
 scales = {'rc_loss': 1e5}
 #####################################
 
-#input the celeb faces directory relative to the cwd
-image_dir='../img_align_celeba'
+if data == 'celeba':
+    #input the celeb faces directory relative to the cwd
+    image_dir='../img_align_celeba'
+    all_image_paths=get_paths(image_dir)
+    train_paths=all_image_paths[:-20000]
+    test_paths=all_image_paths[-20000:]
+    #train set defined in the loop for shuffling
+    test_set=from_path_to_tensor(test_paths, BATCH_SIZE, size=image_size)
 
-all_image_paths=get_paths(image_dir)
-train_paths=all_image_paths[:-20000]
-test_paths=all_image_paths[-20000:]
-#train set defined in the loop for shuffling
-test_set=from_path_to_tensor(test_paths, BATCH_SIZE, size=image_size)
+if data == 'mnist':
+    (train_images, _), (test_images, _) = tf.keras.datasets.mnist.load_data()
+    train_images /= 255.
+    test_images /= 255.
+    TRAIN_BUF = 60000
+    BATCH_SIZE = 128
+    TEST_BUF = 10000
+    train_set = tf.data.Dataset.from_tensor_slices(train_images).shuffle(TRAIN_BUF).batch(BATCH_SIZE)
+    test_set = tf.data.Dataset.from_tensor_slices(test_images).shuffle(TEST_BUF).batch(BATCH_SIZE)
+
 train_dir='./{}/train'.format(DIR)
 test_dir='./{}/test'.format(DIR)
 # check if I'm about to overwrite event files
@@ -42,7 +55,8 @@ train_summary_writer = tf.summary.create_file_writer(train_dir)
 test_summary_writer = tf.summary.create_file_writer(test_dir)
 
 for epoch in range(1,epochs+1):
-    train_set= from_path_to_tensor(train_paths, BATCH_SIZE, size=image_size)
+    if data='celeba':
+        train_set= from_path_to_tensor(train_paths, BATCH_SIZE, size=image_size)
     start_time = time.time()
     for i, batch in enumerate(train_set):
         loss_dict = train_step(batch, model, optimizer, opt2, opt3, mode, scales)
