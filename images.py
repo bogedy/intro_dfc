30e67596b7e03a359845986a2095816763df360e
bogedy <isaiahkriegman@gmail.com>
2019-07-24 21:41:04 -0500

    importing and exporting images

--- images.py
+++ images.py
@@ -1,11 +1,25 @@
 from model import *
 from train_ops import *
 import tensorflow as tf
-
-model = VAE(50, 192, 'vae', 3, loader='experiment18/50')
+from matplotlib import pyplot as plt
+'''
 
 rand_im = tf.random.normal(shape=(1,192,192,3))
 
 _ = compute_loss(model, rand_im, 'vae', {})
 
 tf.saved_model.save(model, './savedmodel')
+'''
+im=''
+imr = tf.io.read_file(im)
+tens = tf.image.decode_image(imr)
+tens=tf.expand_dims(tens, 0)
+
+model = VAE(50, 192, 'vae', 3, loader='experiment18/50')
+
+mean, logvar = model.encode(tens)
+
+out = model.decode(mean)
+
+enc=tf.image.encode_jpeg(out[0])
+tf.write_file('out.jpg', enc)
