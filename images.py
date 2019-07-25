0b3e01be4c139fcbb3c7910b720da948a6ee8d70
Chameleon Cloud User <cc@final.novalocal>
2019-07-25 03:02:14 +0000

    interesting latent space results

--- images.py
+++ images.py
@@ -1,3 +1,4 @@
+import os
 os.environ["CUDA_VISIBLE_DEVICES"]="3"
 
 from model import *
@@ -12,9 +13,9 @@ _ = compute_loss(model, rand_im, 'vae', {})
 
 tf.saved_model.save(model, './savedmodel')
 '''
-im=''
+im='noise.png'
 imr = tf.io.read_file(im)
-tens = tf.image.decode_image(imr)
+tens = tf.image.decode_image(imr, dtype=tf.float32)
 tens=tf.expand_dims(tens, 0)
 
 model = VAE(50, 192, 'vae', 3, loader='experiment18/50')
@@ -23,5 +24,8 @@ mean, logvar = model.encode(tens)
 
 out = model.decode(mean)
 
+out=tf.image.convert_image_dtype(out, tf.uint8)
+
+
 enc=tf.image.encode_jpeg(out[0])
-tf.write_file('out.jpg', enc)
+tf.io.write_file('out.jpg', enc)
